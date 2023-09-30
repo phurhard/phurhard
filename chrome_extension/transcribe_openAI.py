69c0bd024f762cc9eceb6c83a76c898de1b0e3bf
phurhard <phurhardeen@gmail.com>
2023-09-30 23:51:12 +0100

    transcribingWIP

--- chrome_extension/transcribe_openAI.py
+++ chrome_extension/transcribe_openAI.py
@@ -8,9 +8,18 @@ import subprocess
 video = "ayilara.mp4"
 audio = "ayi.mp3"
 ffmpeg_command = ['ffmpeg', '-i', video, '-vn', '-acodec', 'libmp3lame', audio,]
-ffmpeg_process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, sderr=subprocess.PIPE)
+ffmpeg_process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 openai.api_key = "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"
 #os.getenv("OPENAI_API_KEY")
-audio_file = open("ayilara.mp4", "rb")
-transcript = openai.Audio.translate("whisper-1", audio_file)
-print(transcript)
+ffmpeg_process.wait()
+return_code = ffmpeg_process.poll()
+if return_code is None:
+    print("Conversion still in progress")
+else:
+    if return_code == 0:
+        print("Conversion completed successfully")
+        audio_file = open("ayi.mp3", "rb")
+        transcript = openai.Audio.translate("whisper-1", audio_file)
+        print(transcript)
+    else:
+        print("Conversion failed with return code:", return_code)
